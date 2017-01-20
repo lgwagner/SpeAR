062ab4670dafe325ac8b22d9514f7ee9ec8e9415
Lucas Wagner <lucas.wagner@rockwellcollins.com>
2017-01-20 10:36:07 -0600

    removing unnecessary file

--- com.rockwellcollins.spear.translate/src/com/rockwellcollins/spear/translate/transformations/RemoveLustreKeywords.java
+++ com.rockwellcollins.spear.translate/src/com/rockwellcollins/spear/translate/transformations/RemoveLustreKeywords.java
@@ -3,6 +3,7 @@ package com.rockwellcollins.spear.translate.transformations;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeSet;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.EcoreUtil2;
@@ -10,10 +11,52 @@ import org.eclipse.xtext.util.SimpleAttributeResolver;
 
 import com.rockwellcollins.spear.translate.intermediate.PatternDocument;
 import com.rockwellcollins.spear.translate.intermediate.SpearDocument;
-import com.rockwellcollins.spear.utilities.LustreUtilities;
 
 public class RemoveLustreKeywords {
 
+	public static Set<String> getLustreKeywordSet() {
+		Set<String> keywords = new TreeSet<>();
+		keywords.add("then");
+		keywords.add("type");
+		keywords.add("pre");
+		keywords.add("struct");
+		keywords.add("true");
+		keywords.add("else");
+		keywords.add("tel");
+		keywords.add("condact");
+		keywords.add("let");
+		keywords.add("and");
+		keywords.add("var");
+		keywords.add("floor");
+		keywords.add("int");
+		keywords.add("if");
+		keywords.add("not");
+		keywords.add("of");
+		keywords.add("subrange");
+		keywords.add("real");
+		keywords.add("enum");
+		keywords.add("assert");
+		keywords.add("returns");
+		keywords.add("mod");
+		keywords.add("const");
+		keywords.add("div");
+		keywords.add("bool");
+		keywords.add("xor");
+		keywords.add("false");
+		keywords.add("node");
+		keywords.add("or");
+		
+		//these aren't Lustre keywords, but ones we're reserving for the translation
+		keywords.add("initially");
+		keywords.add("once");
+		keywords.add("historically");
+		keywords.add("since");
+		keywords.add("triggers");
+		keywords.add("responds_within");
+		keywords.add("counter");
+		return keywords;
+	}
+	
 	public static Map<EObject,Map<String,String>> transform(SpearDocument doc) {
 		Map<EObject,Map<String,String>> map = new HashMap<>();
 		doc.patterns.values().stream().forEach(p -> map.put(p, transform(p)));
@@ -35,7 +78,7 @@ public class RemoveLustreKeywords {
 	private final Set<String> keywords;
 	
 	public RemoveLustreKeywords() {
-		keywords = LustreUtilities.getLustreKeywordSet();
+		keywords = RemoveLustreKeywords.getLustreKeywordSet();
 	}
 	
 	private boolean checkForConflict(String name) {
